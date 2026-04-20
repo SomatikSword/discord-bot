ebee938a38eeced7a91484a35d49187ba87a31d3
Anastasiya Surzhikova <surzhikova94@inbox.ru>
2026-04-21 01:33:34 +0300

    resolve merge conflict

--- bot.py
+++ bot.py
@@ -5,26 +5,23 @@ import os
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = 561267967946391583
 
-# Intent'ы
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# --- Бот запустился ---
 @bot.event
 async def on_ready():
     print(f"Бот {bot.user} готов к работе!")
 
-# --- Приветствие нового участника ---
 @bot.event
 async def on_member_join(member):
     channel = member.guild.system_channel
 
     if channel is not None:
         await channel.send(
-            f"👋 Добро пожаловать, {member.mention}! Рады видеть тебя на сервере."
+            f"👋 Добро пожаловать, {member.mention}!"
         )
 
         await channel.send(
@@ -32,5 +29,4 @@ async def on_member_join(member):
             "Как добавишь — выдам права на приватную часть сервера, где происходит основной движ."
         )
 
-# --- запуск ---
 bot.run(TOKEN)
